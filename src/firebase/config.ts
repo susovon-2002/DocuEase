213ace6ee39518adc506b116fce49a399fe14af1
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-06 00:06:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-539588255-cbce4",
+  "appId": "1:113974892363:web:1394492bcc200463b9face",
+  "apiKey": "AIzaSyBP43FZiMQnn919Q5gKC-pwA6huJqemUKs",
+  "authDomain": "studio-539588255-cbce4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "113974892363"
+};
