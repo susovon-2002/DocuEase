856244afc620325ff285b9e25920fd2e9e5ac8d7
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-08 23:29:44 +0000

    now

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-539588255-cbce4",
-  "appId": "1:113974892363:web:1394492bcc200463b9face",
+  "appId": "1:113974892363:web:6a5936c071fad21db9face",
   "apiKey": "AIzaSyBP43FZiMQnn919Q5gKC-pwA6huJqemUKs",
   "authDomain": "studio-539588255-cbce4.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "113974892363"
+  "messagingSenderId": "113974892363",
+  "storageBucket": "studio-539588255-cbce4.firebasestorage.app"
 };
