31b27ed9536a9c4ccd9fd4d6d053174befca8b29
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-18 23:08:18 +0000

    Try fixing this error: `Build Error: Error evaluating Node.js code. Erro

--- src/app/remove-pages/page.tsx
+++ src/app/remove-pages/page.tsx
@@ -2,15 +2,12 @@
 
 import { useState } from 'react';
 import { RemovePagesClient } from './RemovePagesClient';
-import { ToolAuthWrapper } from '@/components/ToolAuthWrapper';
 
 export default function RemovePagesPage() {
   const [pageCount, setPageCount] = useState(0);
   return (
       <div className="py-12">
-        <ToolAuthWrapper pageCount={pageCount}>
           <RemovePagesClient onPageCountChange={setPageCount} />
-        </ToolAuthWrapper>
       </div>
   );
 }
