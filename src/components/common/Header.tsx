bce5b7c9d07b7358f4dd525e298ce6b19e4ec24d
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-18 17:40:43 +0000

    more big

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -115,11 +115,11 @@ const AuthContent = () => {
 const Header = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center">
+      <div className="container flex h-20 items-center">
         <div className="mr-4 flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
-            <FileCog className="h-8 w-8 text-primary" />
-            <span className="font-bold text-xl">DocuEase</span>
+            <FileCog className="h-10 w-10 text-primary" />
+            <span className="font-bold text-3xl">DocuEase</span>
           </Link>
         </div>
 
