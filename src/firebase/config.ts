c4d8062544ca929a31c7f316fbcf1cedfdbba747
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-18 19:38:09 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyAZ191522E9Nae9s-Cq_xGPUr3j4A7_gM4",
-  authDomain: "studio-539588255-cbce4.firebaseapp.com",
-  projectId: "studio-539588255-cbce4",
-  storageBucket: "studio-539588255-cbce4.appspot.com",
-  messagingSenderId: "539588255",
-  appId: "1:539588255:web:355322986f334a17154388",
+  "projectId": "studio-539588255-cbce4",
+  "appId": "1:113974892363:web:7611a26b03e45041b9face",
+  "apiKey": "AIzaSyBP43FZiMQnn919Q5gKC-pwA6huJqemUKs",
+  "authDomain": "studio-539588255-cbce4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "113974892363"
 };
