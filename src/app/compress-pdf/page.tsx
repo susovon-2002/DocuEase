d57a8914523a3eeaf525ba3ab7089ad22bb2911a
Susovon 3124 <susovonsantra4@gmail.com>
2025-10-18 23:12:48 +0000

    what is the problem bro why it look like this

--- src/app/compress-pdf/page.tsx
+++ src/app/compress-pdf/page.tsx
@@ -2,13 +2,16 @@
 
 import { useState } from 'react';
 import { CompressPdfClient } from './CompressPdfClient';
+import { ToolAuthWrapper } from '@/components/ToolAuthWrapper';
 
 export default function CompressPdfPage() {
   const [pageCount, setPageCount] = useState(0);
 
   return (
       <div className="py-12">
+        <ToolAuthWrapper pageCount={pageCount}>
           <CompressPdfClient onPageCountChange={setPageCount} />
+        </ToolAuthWrapper>
       </div>
   );
 }
